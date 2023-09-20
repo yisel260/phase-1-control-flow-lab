4b4456ad5caed7536744ad2ef623a0639dea7cd8
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2023-09-19 22:10:18 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
+  if (ride < 400) {
+    return "This one is on me!"
+  }
+  else if (ride >= 400 && ride <2000){
+    return "That will be twenty bucks."
+  }
+  else if (ride >2000 && ride < 2500){
+    return "I will gladly take your thirty bucks."
+  }
+  else if (ride > 2500 ){
+    return 'No can do.'
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+ return city === "NYC"? 'Ok, sounds good.':'No go.'
   // Write your code here!
 }
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case "generous":
+       return "Thank you so much."
+    case "not as generous":
+      return "Thank you."
 
-function switchOnCharmFromTip(){
+    default:
+      return "Bye."
+  
+  }
   // Write your code here!
 }
